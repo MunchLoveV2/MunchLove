5013a16ac0ce11cefc2323d30ae4bc5c8ebf6164
uglymushroom <lifanjin@ucla.edu>
2018-09-09 17:39:00 -0700

    whooops didnt push all htmlroute updates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,27 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
+  // Load signup page
+  app.get("/signup", function(req, res) {
     db.Userinfo.findAll({}).then(function(dbUserinfos) {
-      res.render("index", {
-        msg: "Welcome to the MunchLovers!",
+      res.render("signup", {
+        msg: "Join the MunchLovers!",
         userinfos: dbUserinfos
       });
     });
   });
-
+  // load login page
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+  // load about us
+  app.get("/aboutus", function(req, res) {
+    res.render("aboutus");
+  });
+  // search results page
+  app.get("/searchresults", function(req, res) {
+    res.render("searchresults");
+  });
   // Load example page and pass in an example by id
   app.get("/userinfo/:id", function(req, res) {
     db.Userinfo.findOne({ where: { id: req.params.id } }).then(function(dbUserinfo) {
@@ -19,7 +30,10 @@ module.exports = function(app) {
       });
     });
   });
-
+  // This links to the front page
+  app.get("/", function(req, res) {
+    res.render("frontpage");
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
