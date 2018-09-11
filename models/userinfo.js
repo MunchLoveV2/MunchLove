05736bfd2792b127305f652c17829b99e09748be
uglymushroom <lifanjin@ucla.edu>
2018-09-10 20:00:37 -0700

    jk this is the right commit

--- models/userinfo.js
+++ models/userinfo.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Userinfo = sequelize.define("Userinfo", {
     username: DataTypes.TEXT,
     password: DataTypes.TEXT,
-    salt: DataTypes.TEXT,
+    // salt: DataTypes.TEXT,
     createdAt: DataTypes.DATE
   });
   return Userinfo;
