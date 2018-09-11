722b7aadc5b44cea8849d3e2f911a8a6f6986fe0
uglymushroom <lifanjin@ucla.edu>
2018-09-10 19:55:26 -0700

    pushing up working version before possibly destroying it with password auth stuff

--- models/userinfo.js
+++ models/userinfo.js
@@ -1,7 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var Userinfo = sequelize.define("Userinfo", {
     username: DataTypes.TEXT,
-    password: DataTypes.TEXT
+    password: DataTypes.TEXT,
+    salt: DataTypes.TEXT,
+    createdAt: DataTypes.DATE
   });
   return Userinfo;
 };
