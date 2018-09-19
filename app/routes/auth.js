452329afe2dfefcc63c170c9b7dd180f869fb68e
uglymushroom <lifanjin@ucla.edu>
2018-09-18 18:41:01 -0700

    hi

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,6 +5,43 @@ module.exports = function(app, passport) {
   app.get("/signup", checkLogIn, authController.signup);
   app.get("/login", checkLogIn, authController.login);
   app.get("/aboutus", authController.aboutus);
+  app.post("/frontpage", checkZip, authController.searchresults);
+  app.post('/frontpage', (req, res) => {
+    const location = req.body.text;
+    req.session.location = location;
+    util.yelpSearch(location)
+      .then((result) => {
+        const businessArr = [];
+        const results = result.data.search;
+        if (result.errors || results.total === 0) {
+          console.log(`No businesses found at location: ${location}`);
+          res.status(204).send(businessArr);
+        } else {
+          results.business.forEach((store) => {
+            console.log('returned store', store);
+            const storeData = {
+              name: store.name,
+              place_id: store.id,
+              address: store.location.formatted_address.split('\n').join(', '),
+              phone: store.display_phone,
+              website: store.url.split('?')[0],
+              photos: store.photos[0],
+              price: store.price,
+              rating: store.rating,
+              latitude: store.coordinates.latitude,
+              longitude: store.coordinates.longitude
+            };
+            businessArr.push(storeData);
+          });
+          res.status(200).send(businessArr);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send('Failed to retrieve business data from Yelp API');
+      });
+
+
   app.get("/searchresults", authController.searchresults);
   app.post(
     "/signup",
--- app/routes/yelp.js
+++ app/routes/yelp.js
@@ -1,28 +1,31 @@
-module.exports = function(yelp, userfavorite) {
-var params = {};
-yelp.use(
-  new params(
-    {location: "zip"}
-  ),
-  yelp.query(
-    'businesses/search', 
-    params.location
-  )
-).then(data => {
-  var dataArray = JSON.parse(data).businesses;
-  var yelpData = { 
-    businesses: []
-  }
-  dataArray.forEach(data => {
-    var dataObject = {
-      name: data.name,
-      rating: data.rating,
-      phone: data.phone,
-      image: data.image_url,
-      id: data.id
-    };
-  yelpData.businesses.push(dataObject);
+var yelpAPI = require('yelp-api');
+var apiKey = '81fLxhTR0I7D6azLHNAUlu88BxvFgonIl8rD-oguXUftxtdkI5DjI0AB8SEQ1w2uG3N5WobKaHuyY-Hng_jhLPFHYeuptXgzycy2gEbJxg-V_TU8wJ4A35ASpsWVW3Yx';
+
+module.exports = function(req, res) {
+  console.log("HELLO I AM THE REQ >>>>>>>>", req);
+  var yelp = new yelpAPI(apiKey);
+  var zip = JSON.parse(req);
+  var params = [{ location: zip }];
+  yelp.query('businesses/search', params)
+  .then(data => {
+    var dataArray = JSON.parse(data).businesses;
+    var yelpData = { 
+      businesses: []
+    }
+
+    dataArray.forEach(business => {
+      var dataObject = {
+        name: business.name,
+        rating: business.rating,
+        phone: business.phone,
+        image: business.image_url,
+        id: business.id
+      }
+      yelpData.businesses.push(dataObject);
+    })
   })
-})
+  .catch(err => {
+    console.log(err);
+  });
 };
 
